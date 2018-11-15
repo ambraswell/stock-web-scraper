d0206025c72687f48762f02c89c9cb5175aac718
ambraswell <aaron.m.braswell@gmail.com>
2018-11-14 22:51:03 -0600

    split out grabbing and printing the data into functions in stock_data

--- stock scraper.py
+++ stock scraper.py
@@ -1,16 +1,24 @@
-from site_html import *
+from stock_data import *
 
-stocks = ["GE"]
+list_of_stocks_to_get = ["GE", "F", "AMZN", "TSLA"]
 
-stocklist_data = []
-for stock in stocks:
-    print(stock)    
-    stockdata = Stock_Data(stock)
-    stocklist_data.append(stockdata)
-for stock in stocklist_data:
-    dir(stock.finviz_table_data)
-    for item in stock.finviz_table_data:
-        print(item + ": " + stock.finviz_table_data[item])
+def retrieve_stock_data(stock):
+    return Stock_Data(stock)
+
+def get_all_stocks_data(stocks):
+    stocks_to_get = []
+    for stock in stocks:
+        print(stock)    
+        stocks_to_get.append(retrieve_stock_data(stock))
+    return stocks_to_get
+
+def print_finviz_table_info_for_stocks(stocklist):
+    for stock in stocklist:
+        for item in stock.finviz_table_data:
+            print(item + ": " + stock.finviz_table_data[item])
+
+stocklist = get_all_stocks_data(list_of_stocks_to_get)
+print_finviz_table_info_for_stocks(stocklist)
     
 
 

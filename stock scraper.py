22f46ccb76ad75bf1f1ed7e19c799b034d9d2c5a
ambraswell <aaron.m.braswell@gmail.com>
2018-11-14 23:36:43 -0600

    added print to csv functionality. Still needs work

--- stock scraper.py
+++ stock scraper.py
@@ -1,6 +1,23 @@
 from stock_data import *
+import csv
+import time
 
-list_of_stocks_to_get = ["GE", "F", "AMZN", "TSLA"]
+list_of_stocks_to_get = ["GE", "F"]
+
+def write_to_csv(stocks):
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    file_name = "stocks for " + timestr + '.csv'
+    myFile = open(file_name, 'w')
+    for stock in stocks:        
+        with open(file_name, 'w') as csvfile:
+            myFields = ['symbol', 'url' ,'key', 'value']
+            writer = csv.DictWriter(myFile, fieldnames=myFields)
+            writer.writeheader()
+            writer.writerow({'symbol' :  stock.symbol, 'url' : stock.finviz_url})
+            for item in stock.finviz_table_data:
+                writer.writerow({'symbol' : '', 'url': '', 'key' : item , 'value': stock.finviz_table_data[item]})
+            writer.writerow({})
+        #myFile.close()
 
 def retrieve_stock_data(stock):
     return Stock_Data(stock)
@@ -18,7 +35,7 @@ def print_finviz_table_info_for_stocks(stocklist):
             print(item + ": " + stock.finviz_table_data[item])
 
 stocklist = get_all_stocks_data(list_of_stocks_to_get)
-print_finviz_table_info_for_stocks(stocklist)
-    
+#print_finviz_table_info_for_stocks(stocklist)
+write_to_csv(stocklist)    
 
 
